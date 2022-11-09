52edd8fe4888bb9dfd80a449b09fc9b4d934065c
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-08 19:19:23 -0500

    react basics

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-basics/src/components/NewExpense/ExpenseForm.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-basics/src/components/NewExpense/ExpenseForm.js
@@ -5,6 +5,7 @@ const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('')
     const [enteredAmount, setEnteredAmount] = useState('')
     const [enteredDate, setEnteredDate] = useState('')
+    const [isEditing, Editing] = useState(false)
     // const [userInput, setUserInput] = useState({
     //     enteredTitle: '',
     //     enteredAmount: '',
@@ -38,42 +39,65 @@ const ExpenseForm = (props) => {
         // })
     };
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        const expenseData = {
-            title: enteredTitle,
-            amount: enteredAmount,
-            date: new Date(enteredDate)
-        };
+    const startEditing = () => {
+        Editing(true)
+    }
 
-        props.onSaveExpenseData(expenseData);    // sends expenseData to NewExpense.js
-        console.log(expenseData)
-        setEnteredTitle('');            // sets the queries to blank
-        setEnteredAmount('');
-        setEnteredDate('');
+    const stopEditing = () => {
+        Editing(false);
+    }
+
+    const submitHandler = (event) => {
+        if(!isEditing) {
+            Editing(true)
+            event.preventDefault();
+        } else if(isEditing){
+            console.log("A")
+            event.preventDefault();
+            const expenseData = {
+                title: enteredTitle,
+                amount: enteredAmount,
+                date: new Date(enteredDate)
+            };
+
+            props.onSaveExpenseData(expenseData);    // sends expenseData to NewExpense.js
+            console.log(expenseData)
+            setEnteredTitle('');            // sets the queries to blank
+            setEnteredAmount('');
+            setEnteredDate('');
+            Editing(false)
+        }
     };
 
-    return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
-                </div>
-            </div>
+    if(!isEditing) {
+        return(
             <div className="new-expense__actions">
-                <button type="submit">Add Expense</button>
+                <button onClick={() => Editing(true)}>Add an Expense</button>
             </div>
-        </form>
-    );
+        )
+    }else{
+        return (
+            <form onSubmit={submitHandler}>
+                <div className="new-expense__controls">
+                    <div className="new-expense__control">
+                        <label>Title</label>
+                        <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
+                    </div>
+                    <div className="new-expense__control">
+                        <label>Amount</label>
+                        <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
+                    </div>
+                    <div className="new-expense__control">
+                        <label>Date</label>
+                        <input type="date" min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
+                    </div>
+                </div>
+                <div className="new-expense__actions">
+                    <button onClick={() => Editing(false)}>Cancel</button>
+                    <button type="submit">Add Expense</button>
+                </div>
+            </form>)
+    }
 }
 
 export default ExpenseForm;
