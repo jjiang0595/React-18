6f04111e065068a1964fcbf803713d1e621a953c
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-09 12:52:36 -0500

    react basics

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-basics/src/components/Chart/Chart.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-basics/src/components/Chart/Chart.js
@@ -2,12 +2,17 @@ import './Chart.css'
 import ChartBar from './ChartBar'
 
 const Chart = props => {
+    const dataPointValues = props.dataPoints.map(dataPoint =>{
+        return dataPoint.value
+    });
+    const totalMaximum = Math.max(...dataPointValues);
+
     return <div className="chart">
         {props.dataPoints.map(dataPoint =>
             <ChartBar
                 key={dataPoint.label}
                 value={dataPoint.value}
-                maxValue={null}
+                maxValue={totalMaximum}
                 label={dataPoint.label}/>)}
     </div>
 };
--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-basics/src/components/Expenses/Expenses.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-basics/src/components/Expenses/Expenses.js
@@ -1,9 +1,10 @@
 import './Expenses.css'
 import Card from '../UI/Card'
-import ExpenseItem from "./ExpenseItem";
+
 import {useState} from "react";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
     const [filteredYear, setFilteredYear] = useState('2020')
@@ -20,6 +21,7 @@ function Expenses(props) {
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpensesChart expenses={filteredExpenses} />
                 <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
