f0913c32d79e0b2ccb8c3e241a62f2a3ef076854
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-15 13:29:57 -0500

    food order project

---
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/food-order/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import classes from './Header.module.css'
+import meals from '../../assets/meals.jpg'
+import MealsSummary from "./MealsSummary";
+
+const Header = (props) => {
+    return (
+        <>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>
+                <img src={meals} alt="A table full of delicious food"/>
+            </div>
+            <MealsSummary />
+        </>
+    )
+}
+
+export default Header
