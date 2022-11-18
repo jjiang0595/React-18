1080d156bd425124ef1b9fb4663c0cee57f50198
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-18 13:16:17 -0500

    react memo

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-optimization/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-optimization/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import Button from "./components/UI/Button/Button";
 import './App.css';
+import DemoOutput from "./components/UI/Demo/DemoOutput";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+    const [showParagraph, setShowParagraph] = useState(false)
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+
+    return (
+        <div className="app">
+            <h1>Hi there!</h1>
+            <DemoOutput show={showParagraph} />
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+        </div>
+    );
 }
 
 export default App;
