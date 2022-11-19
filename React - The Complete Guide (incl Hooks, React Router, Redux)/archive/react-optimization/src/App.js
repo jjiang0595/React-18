9f2bb1b20b91268d2ec4b13cf4e7d35a04313eb8
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-19 12:55:42 -0500

    setup

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-optimization/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-optimization/src/App.js
@@ -1,19 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import Button from "./components/UI/Button/Button";
 import './App.css';
 import DemoOutput from "./components/UI/Demo/DemoOutput";
 
 function App() {
     const [showParagraph, setShowParagraph] = useState(false)
+    const [allowToggle, setAllowToggle] = useState(false)
 
-    const toggleParagraphHandler = () => {
-        setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    const toggleParagraphHandler = useCallback(() => {
+        if (allowToggle) {
+            setShowParagraph(prevShowParagraph => !prevShowParagraph);
+        }
+    }, [allowToggle])
+
+    const allowToggleHandler = () => {
+        setAllowToggle(true);
     }
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
             <DemoOutput show={showParagraph} />
+            <Button onClick={allowToggleHandler}>Allow Toggling</Button>
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
         </div>
     );
