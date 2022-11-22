7c41fb9f5c8f64d1104b30fd5c7ed23649d5d9b3
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-21 21:48:11 -0500

    refactoring

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-forms/src/components/SimpleInput.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-forms/src/components/SimpleInput.js
@@ -1,29 +1,30 @@
-import {useRef, useState} from 'react'
+import {useState} from 'react'
 
 const SimpleInput = (props) => {
-    const nameInputRef = useRef()
     const [enteredName, setEnteredName] = useState('')
-    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
     const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+
+    const enteredNameIsValid = enteredName.trim() !== '';
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
 
     const nameInputChangeHandler = event => {
         setEnteredName(event.target.value)
     }
 
+    const nameInputBlurHandler = event => {
+        setEnteredNameTouched(true)
+    }
+
     const formSubmissionHandler = event => {
         event.preventDefault()
         setEnteredNameTouched(true)
-        if (enteredName.trim() == '') {
-            setEnteredNameIsValid(false)
+        if (!enteredNameIsValid) {
             return;
         }
-        setEnteredNameIsValid(true)
-        const enteredValue = nameInputRef.current.value
-        console.log(enteredValue)
         setEnteredName('')
+        setEnteredNameTouched(false)
     }
 
-    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
     const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
 
   return (
@@ -31,10 +32,10 @@ const SimpleInput = (props) => {
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input
-            ref={nameInputRef}
             type='text'
             id='name'
             onChange={nameInputChangeHandler}
+            onBlur = {nameInputBlurHandler}
             value={enteredName}
         />
           {nameInputIsInvalid && <p className="error-text">Name must not be empty.</p>}
