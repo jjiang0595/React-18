1f7a1bd909fda8f141f64bde8c26a92f4b234ca8
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-27 13:43:54 -0500

    setup

---
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux/src/components/Header.js
@@ -0,0 +1,35 @@
+import classes from './Header.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {authActions} from "../store/auth";
+
+const Header = () => {
+  const dispatch = useDispatch()
+  const auth = useSelector(state => state.auth.isAuthenticated)
+
+  const logout = () => {
+    dispatch(authActions.logout())
+  }
+
+  const loggedInContent =  <nav>
+    <ul>
+      <li>
+        <a href='/'>My Products</a>
+      </li>
+      <li>
+        <a href='/'>My Sales</a>
+      </li>
+      <li>
+        <button onClick={logout}>Logout</button>
+      </li>
+    </ul>
+  </nav>
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {auth && loggedInContent}
+    </header>
+  );
+};
+
+export default Header;
