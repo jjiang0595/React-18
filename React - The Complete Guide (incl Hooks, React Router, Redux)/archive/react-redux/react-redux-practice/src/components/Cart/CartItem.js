35003a0d3d3a8a7915b11834ae896c708d2e7acb
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-27 18:38:48 -0500

    fixing badge

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/components/Cart/CartItem.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/components/Cart/CartItem.js
@@ -3,7 +3,7 @@ import {cartActions} from "../../store/cartSlice";
 import {useDispatch} from "react-redux";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price, id } = props.item;
+    const {title, quantity, total, price, id} = props.item;
     const dispatch = useDispatch()
 
     const addItemHandler = () => {
@@ -14,27 +14,30 @@ const CartItem = (props) => {
         }))
     }
 
+    const removeItemHandler = () => {
+        dispatch(cartActions.removeItem(id))
+    }
 
     return (
-    <li className={classes.item}>
-      <header>
-        <h3>{title}</h3>
-        <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
-        </div>
-      </header>
-      <div className={classes.details}>
-        <div className={classes.quantity}>
-          x <span>{quantity}</span>
-        </div>
-        <div className={classes.actions}>
-          <button>-</button>
-          <button onClick={addItemHandler}>+</button>
-        </div>
-      </div>
-    </li>
-  );
+        <li className={classes.item}>
+            <header>
+                <h3>{title}</h3>
+                <div className={classes.price}>
+                    ${total.toFixed(2)}{' '}
+                    <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+                </div>
+            </header>
+            <div className={classes.details}>
+                <div className={classes.quantity}>
+                    x <span>{quantity}</span>
+                </div>
+                <div className={classes.actions}>
+                    <button onClick={removeItemHandler}>-</button>
+                    <button onClick={addItemHandler}>+</button>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default CartItem;
--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/store/cartSlice.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/store/cartSlice.js
@@ -20,11 +20,23 @@ const cartSlice = createSlice({
                     totalPrice: newItem.price
                 })
             } else {
-                existingItem.amount++
+                existingItem.quantity++
                 existingItem.totalPrice += newItem.price
             }
             console.log(state.items)
+        },
+        removeItem(state, action) {
+            const id = action.payload
+            const existingItem = state.items.find(item => item.id === id)
+            state.totalQuantity--
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id)
+            } else {
+                existingItem.quantity--
+                existingItem.totalPrice -= existingItem.price
+            }
         }
+
     }
 })
 
