b9152d183dcd83d2d04150207b34e1aca034c111
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-27 20:18:12 -0500

    sending data to backend

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/App.js
@@ -2,16 +2,26 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
     const showCart = useSelector(state => state.ui.showVisibility)
+    const cart = useSelector(state => state.cart)
 
-  return (
-    <Layout>
-        {showCart && <Cart />}
-      <Products />
-    </Layout>
-  );
+    useEffect(() => {
+        fetch('https://react-http-1870f-default-rtdb.firebaseio.com/cart.json', {
+            method: 'PUT',
+            body: JSON.stringify(cart)
+        })
+    }, [cart])
+
+
+    return (
+        <Layout>
+            {showCart && <Cart/>}
+            <Products/>
+        </Layout>
+    );
 }
 
 export default App;
