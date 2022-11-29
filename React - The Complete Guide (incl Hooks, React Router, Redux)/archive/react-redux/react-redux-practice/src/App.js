cb8201183fc00bda5bd22c88f8990fb484a40470
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-28 22:58:36 -0500

    action creator

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/App.js
@@ -3,8 +3,8 @@ import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
-import {uiActions} from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import {sendCartData} from "./store/cartSlice";
 
 let isInitial = true;
 
@@ -15,42 +15,11 @@ function App() {
     const notification = useSelector(state => state.ui.notification)
 
     useEffect( () => {
-        const sendCartData = async () => {
-            dispatch(uiActions.showNotification({
-                status: 'pending',
-                title: 'Sending...',
-                message: 'Sending cart data!'
-            }))
-
-            const response = await fetch('https://react-http-1870f-default-rtdb.firebaseio.com/cart.json', {
-                method: 'PUT',
-                body: JSON.stringify(cart)
-            })
-
-            if (!response.ok) {
-                throw new Error('Sending cart data failed.')
-            }
-
-            dispatch(uiActions.showNotification({
-                status: 'success',
-                title: 'Success!',
-                message: 'Send cart data successfully!'
-            }))
-
-        }
-
         if (isInitial) {
             isInitial = false
             return
         }
-
-        sendCartData().catch(error => {
-            dispatch(uiActions.showNotification({
-                status: 'error',
-                title: 'Error!',
-                message: 'Sending cart data failed!'
-            }))
-        })
+        dispatch(sendCartData(cart))
     }, [cart, dispatch])
 
 
