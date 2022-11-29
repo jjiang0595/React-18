fac2f6679ddc6359c7a163ce7dd7c94697f377ce
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-28 23:12:45 -0500

    fetching logic

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/App.js
@@ -4,7 +4,7 @@ import Products from './components/Shop/Products';
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import Notification from "./components/UI/Notification";
-import {sendCartData} from "./store/cartSlice";
+import {fetchCartData, sendCartData} from "./store/cartActions";
 
 let isInitial = true;
 
@@ -13,6 +13,10 @@ function App() {
     const showCart = useSelector(state => state.ui.showVisibility)
     const cart = useSelector(state => state.cart)
     const notification = useSelector(state => state.ui.notification)
+
+    useEffect(() => {
+        dispatch(fetchCartData())
+    }, [dispatch])
 
     useEffect( () => {
         if (isInitial) {
