a6649fc027d034020e75446d914c7d0132571f7d
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-28 21:35:03 -0500

    handling http states

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/store/ui-slice.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-redux/react-redux-practice/src/store/ui-slice.js
@@ -3,12 +3,21 @@ import {createSlice} from "@reduxjs/toolkit";
 const uiSlice = createSlice({
     name: 'cart',
     initialState: {
-        showVisibility: false
+        showVisibility: false,
+        notification: null
     },
     reducers: {
         toggleCart(state) {
             state.showVisibility = !state.showVisibility
         },
+        showNotification(state, action) {
+            state.notification =
+                {
+                    status: action.payload.status,
+                    title: action.payload.title,
+                    message: action.payload.message
+                }
+        }
     }
 })
 
