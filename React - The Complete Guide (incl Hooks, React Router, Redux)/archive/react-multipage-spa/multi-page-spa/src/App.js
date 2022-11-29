abd3153143d3eb262dacc6971f1cfe538f5313fc
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-29 16:54:42 -0500

    switch and exact

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-multipage-spa/multi-page-spa/src/App.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-multipage-spa/multi-page-spa/src/App.js
@@ -1,4 +1,4 @@
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -7,13 +7,13 @@ import ProductDetail from "./pages/ProductDetail";
 function App() {
     return (
         <div>
-            <header>
-                <MainHeader />
-            </header>
+            <MainHeader/>
             <main>
-                <Route path="/welcome"><Welcome/></Route>
-                <Route path="/products"><Products/></Route>
-                <Route path="/product-detail/:productId"><ProductDetail /></Route>
+                <Switch>
+                    <Route path="/welcome"><Welcome/></Route>
+                    <Route path="/products" exact><Products/></Route>
+                    <Route path="/products/:productId"><ProductDetail/></Route>
+                </Switch>
             </main>
         </div>
     );
