562ae5a0c71e5d98f0a07731a75a0706ac441395
Jerry Jiang <jerryjiang49@gmail.com>
2022-11-30 12:57:50 -0500

    not found page

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-multipage-spa/practice-project/src/pages/QuoteDetail.js
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/react-multipage-spa/practice-project/src/pages/QuoteDetail.js
@@ -1,13 +1,24 @@
 import {Route, useParams} from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Max', text: 'Learning React is fun!'},
+    {id: 'q2', author: 'Maximilian', text: 'Learning React is great!'},
+]
 
 const QuoteDetail = () => {
     const params = useParams();
 
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+    if (!quote) {
+        return <p>No quote found!</p>
+    }
+
     return (
         <>
-            <p>Quote Detail</p>
-            <p>{params.quoteId}</p>
+            <HighlightedQuote text={quote.text} author={quote.author} />
             <Route path="/quotes/:quoteId/comments"><Comments /></Route>
         </>
     )
