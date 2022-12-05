6641f0a5083c51c5a74168532d87065a1a5901ac
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-05 16:02:38 -0500

    custom components

---
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/nextjs/nextjs-project/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+    return <MeetupDetail
+            image='https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Pyramids_of_the_Giza_Necropolis.jpg/1920px-Pyramids_of_the_Giza_Necropolis.jpg'
+            title="A First Meetup"
+            address="Some Street 5, Some City"
+            description="The meetup description"
+        />
+}
+
+export default MeetupDetails
