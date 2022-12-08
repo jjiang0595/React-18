cb0e41374b7d0a80eb7344c6edeae460de7e5e68
Jerry Jiang <jerryjiang49@gmail.com>
2022-12-08 16:30:45 -0500

    refs

--- React - The Complete Guide (incl Hooks, React Router, Redux)/archive/misc/typescript-project/src/components/NewTodo.tsx
+++ React - The Complete Guide (incl Hooks, React Router, Redux)/archive/misc/typescript-project/src/components/NewTodo.tsx
@@ -1,11 +1,15 @@
 import React, {useRef} from "react";
 export const NewTodo = () => {
-    const todoTextInputRef = useRef();
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault()
 
+        const enteredText = todoTextInputRef.current!.value
 
+        if (enteredText.trim().length === 0) {
+            return;
+        }
     }
 
 
